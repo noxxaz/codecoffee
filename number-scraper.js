dc877e2832f3765fb2d2fae358ce220d3e3ab2be
Zach Brown <zach@zach.tokyo>
2022-07-12 10:47:57 +0900

    Committing unsaved changes

--- number-scraper.js
+++ number-scraper.js
@@ -11,7 +11,7 @@ const numScraper = (str) => {
 
   // split input string into array of numbers
   const nums = str
-    .replace(/\D/g, ' ')
+    .replace(/\D/g, ' ') // replace all non-numeric chars with spaces
     .split(' ')
     .map(element => parseInt(element))
     .filter(element => !isNaN(element));
@@ -21,7 +21,6 @@ const numScraper = (str) => {
 
 };
 
-
 numScraper("12 34 56, 78, and 9") // [12, 34, 56, 78, 9]
 numScraper("I ate 15 bananas and got 1 tummyache") // [15, 1]
-numScraper("Y0L0!!!!1") // [0,0,1]
+numScraper("Y0L0!!!!1") // [0, 0, 1]
