70da86e4a457ab329bf228e1c16865a6e087777e
Zach Brown <zach@zach.tokyo>
2022-07-14 10:17:33 +0900

    refactored makeGrid to solve by reference issue

--- makeGrid.js
+++ makeGrid.js
@@ -1,20 +1,37 @@
+// Define a function called makeGrid.
+// makeGrid should take in an number n and some primitive value.
+// makeGrid should create a grid (an array of arrays), with the dimensions of n by n.
+// Return the grid filled with the given value.
+//  const makeGrid = (n, value) => {
+//   const outerArray = new Array(n);
+//   const innerArray = [];
+
+
 /*
  * Creates an n by n grid (array of arrays) filled with a repeated value in each cell.
  * @param {number} n the side length of the grid
  * @param {Object} value A value to populate the grid with
  * @returns {Array<Array<Object>>} an n by n grid
  */
- const makeGrid = (n, value) => {
-  const outerArray = [];
-  const innerArray = [];
- 
-  // build inner array (once)
-  for (let i = 0; i < n; i++) {
-  	innerArray.push(value);
-  }
-  // push inner array into outer array n times
-  for (let j = 0; j < n; j++) {
-    outerArray.push(innerArray);
-  }
-  return outerArray;
+  const makeGrid = (n, value) => {
+    const outerArray = new Array(n);
+    const innerArray = [];
+   
+    // build empty 2-dimensional array
+    for (let i = 0; i < n; i++) {
+      outerArray[i] = new Array();
+      for (let j = 0; j < n; j++) {
+        outerArray[i].push(value);
+      }
+    // // populate new 2-dimensional array with value
+    // for (const array in outerArray) {
+    //   for (const element in innerArray) {
+    //     element = 'R';
+    //   }
+     }
+  
+    return outerArray;
 };
+
+console.log(makeGrid(6, 'A'));
+console.log(makeGrid(4, '#'));
