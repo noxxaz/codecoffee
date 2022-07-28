0cd72e8571126eb38a6bfcc31d08aee745471d3a
Zach Brown <zach@zach.tokyo>
2022-07-28 10:26:47 +0900

    cleaned up tests

--- inverse-object-filter.js
+++ inverse-object-filter.js
@@ -1,35 +1,12 @@
 /*-----------------------------------------------------------------------------
  *  "Code Coffee" coding challenge
  *  Challenge:  INVERSE OBJECT FILTER
- *  Solution Date: 2022/07/28 Code: 
+ *  Solution Date: 2022/07/28 Code: E52F575B
  *  Zach Brown, Code Chrysalis Immersive (CC27)
  *  
  * Write a function called inverseObjectFilter that takes in an object and an 
  * array of strings. The values in the object are strings. Create a copy of this 
  * object that only contains the keys/values if the value is NOT present in the array.
- *
- * Examples:
- * 
-  let obj = {
-   a: "apple",
-   b: "banana",
-   c: "canteloupe",
-   d: "durian"
-  }; 
-  let newObj = inverseObjectFilter(obj, ["banana", "durian"]);
- * In this code, newObj should be the object { a: "apple", c: "canteloupe" } 
- * because apple and canteloupe where NOT present in the input array.
-
-  let obj = {
-    a: "x",
-    b: "y",
-    x: "a"
-  };
-  let newObj = inverseObjectFilter(obj, ["x"]);
- * In this code, newObj should be the object { b: "y", x: "a" }.
- * a: "x" was NOT included because "x" was in the array. However, 
- * x: "a" WAS included because "a" was not in the array. 
- * The exclusion list only applies to the value, not the key.
  *---------------------------------------------------------------------------*/
   const inverseObjectFilter = (obj, valueExclusionList) => {
     const newObj = {};
@@ -38,36 +15,15 @@
     const values = Object.values(obj);
     const keys = Object.keys(obj);
     for (let i = 0; i < values.length; i++) {
+      // if value is not in exclusion list
       if (!valueExclusionList.includes(values[i])) 
+        // write key/value pair to new object
         newObj[keys[i]] = values[i];
     }
     return newObj;
-    // if ! value exists in exclusion list
-    // write key/value to new object
-
-
-
-    // or
-
-    // deep copy object
-    // loop through values array of object
-    // delete key/value where value matches exclusion list
-
-
-
-    // for (const item of valueExclusionList) {
-
-    //   console.log(obj, " ",item);
-    //   if (obj[item]) {
-    //     newObj[obj[item].key] = obj[item].value;
-    //   }
-    // }
-    // return newObj;
-
-
   };
-//--- TEST CASES --------------------------------------------------------------
 
+//--- TEST CASES --------------------------------------------------------------
 let obj = {
   a: "apple",
   b: "banana",
@@ -75,10 +31,10 @@ let obj = {
   d: "durian"
  }; 
  let newObj = inverseObjectFilter(obj, ["banana", "durian"]);
- console.log(newObj);
+ console.log(newObj); // { a: "apple", c: "canteloupe" }
 
  let newObj2 = inverseObjectFilter(obj, ['banana', 'durian', 'apple', 'canteloupe']);
  console.log(newObj2); // {}
 
  let newObj3 = inverseObjectFilter(obj, []);
- console.log(newObj3); 
+ console.log(newObj3); //{ a: "apple", b: "banana", c: "canteloupe", d: "durian" }
